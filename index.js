e25783c7867074842a4406cc4a3123d2bab4742e
AslanAV <autlevaslan@yandex.ru>
2023-06-28 17:17:13 +0300

    #step 6 - fix lint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import greeting from './src/cli.js';
 import evenGame from './src/games/even.js';
 import calculatorGame from './src/games/calculator.js';
 import gcdGame from './src/games/gcd.js';
-import progression from "./src/games/progression.js";
+import progression from './src/games/progression.js';
 
 export default greeting;
 export { evenGame };
