97880afaf4ce7ff5570b027b3bfba56866d399aa
AslanAV <autlevaslan@yandex.ru>
2023-06-28 19:37:30 +0300

    #step 9 - fix lint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import evenGame from './src/games/even.js';
 import calculatorGame from './src/games/calculator.js';
 import gcdGame from './src/games/gcd.js';
 import progressionGame from './src/games/progression.js';
-import primeGame from './src/games/prime.js'
+import primeGame from './src/games/prime.js';
 
 export default greeting;
 export { evenGame };
