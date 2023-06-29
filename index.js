8d8ccc3e5171b719125a1c8a6c2439dcd579d07b
AslanAV <autlevaslan@yandex.ru>
2023-06-29 04:17:43 +0300

    rename game calculator to calc

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import greeting from './src/cli.js';
 import evenGame from './src/games/even.js';
-import calculatorGame from './src/games/calculator.js';
+import calcGame from './src/games/calc.js';
 import gcdGame from './src/games/gcd.js';
 import progressionGame from './src/games/progression.js';
 import primeGame from './src/games/prime.js';
 
 export default greeting;
 export { evenGame };
-export { calculatorGame };
+export { calcGame };
 export { gcdGame };
 export { progressionGame };
 export { primeGame };
